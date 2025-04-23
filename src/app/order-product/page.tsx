d56027bc8477eab7a1f2fcafa3a38fecb801dc0e
Alecto <fomenkoandrey1978@gmail.com>
2025-04-23 10:46:08 +0300

    Style(tailwind): add button styles and refactor button implementation in order product page

--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -16,18 +16,14 @@ export default function OrderProduct() {
   return (
     <div>
       <h1 className="header-title">Order product</h1>
-      <button
-        onClick={orderProductHandler}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-      >
-        Go to products
-      </button>
-      <button
-        onClick={goToRootPageHandler}
-        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-      >
-        Go to root page
-      </button>
+      <div className="flex justify-center items-center gap-6">
+        <button onClick={orderProductHandler} className="btn btn-primary">
+          Go to products
+        </button>
+        <button onClick={goToRootPageHandler} className="btn btn-secondary">
+          Go to root page
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             pathname === item.href
               ? 'bg-red-600'
               : 'bg-indigo-600 hover:bg-indigo-900 hover:-translate-y-1 hover:scale-115'
-          } block text-white font-medium py-3 px-4 rounded-lg shadow-md transition-all duration-150 focus:outline-none focus:ring-4 focus:ring-indigo-300 focus:ring-opacity-70`}
+          } block text-white font-medium py-3 px-4 rounded-lg shadow-md transition-all duration-150 focus:outline-none focus:ring-4 focus:ring-indigo-300`}
         >
           {item.label}
         </Link>
